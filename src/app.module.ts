b4f9d24b4b7cfa2c81d0a2b3bd8168ff6edcf188
yardcooper <colincrick.lovestobecreative@outlook.com>
2023-07-27 04:09:06 -0400

    added: basic configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { AppConfigModule } from './config/app.config.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [AppConfigModule, CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const httpPort = configService.get<string>('http.port');
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
@@ -14,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(httpPort);
 }
 bootstrap();
