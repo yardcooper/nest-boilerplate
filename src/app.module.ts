0cd0c32475846992e2991bc48f1f7de0cb69ed21
yardcooper <talktorichard39@gmail.com>
2023-09-25 12:14:09 -0400

    updated some configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 import { AppConfigModule } from './config/app.config.module';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+
+import { CatsModule } from './cats/cats.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [AppConfigModule, AuthModule, CatsModule, UsersModule],
